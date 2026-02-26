0816f941152f1b730011e2ea84a0269afc0b5367
amiitdeshmukh-lab <amit.deshmukh@cctech.co.in>
2026-02-26 18:40:49 +0530

    update

--- worker/fib.js
+++ worker/fib.js
@@ -1,6 +1,12 @@
 function fib(index) {
   if (index < 2) return 1;
-  return fib(index - 1) + fib(index - 2);
+  let a = 1, b = 1;
+  for (let i = 2; i <= index; i++) {
+    let temp = a + b;
+    a = b;
+    b = temp;
+  }
+  return b;
 }
 
 module.exports = fib;
